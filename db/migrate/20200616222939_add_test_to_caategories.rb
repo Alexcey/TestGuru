9f67ef417fe2e1f152d98d3f68290f92c8489d84
Alexcey <alexcey007@yandex.ru>
2020-06-17 01:35:31 +0300

    add ref test

---
+++ db/migrate/20200616222939_add_test_to_caategories.rb
@@ -0,0 +1,5 @@
+class AddTestToCaategories < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :tests, :category, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_222542) do
+ActiveRecord::Schema.define(version: 2020_06_16_222939) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2020_06_16_222542) do
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "test_id"
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", force: :cascade do |t|
@@ -38,6 +40,8 @@ ActiveRecord::Schema.define(version: 2020_06_16_222542) do
     t.integer "level", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,4 +52,6 @@ ActiveRecord::Schema.define(version: 2020_06_16_222542) do
   end
 
   add_foreign_key "answers", "questions"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "categories"
 end
