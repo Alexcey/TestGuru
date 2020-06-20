b92cff521bd2d982a2d0e5e4235d60d10043cb5f
Alexcey <alexcey007@yandex.ru>
2020-06-20 04:11:14 +0300

    add methos search in tets

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ApplicationRecord
+  belongs_to :category
   has_many :test_passes
   has_many :users, through: :test_passes
+
+  def category_where_title(title)
+    Test.joins(:category).where('categories.title = ?', title).order(title: :desc)
+  end
 end
