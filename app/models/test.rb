19712aba457d5bc2f93af7bc65468e294105c40c
Alexcey <alexcey007@yandex.ru>
2020-06-20 15:35:11 +0300

    add title

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,6 +4,6 @@ class Test < ApplicationRecord
   has_many :users, through: :test_passes
 
   def category_where_title(title)
-    Test.joins(:category).where('categories.title = ?', title).order(title: :desc)
+    Test.joins(:category).where('categories.title = ?', title).order(title: :desc).pluck(:title)
   end
 end
