70be74158fe0135e165da5d444bbdffb28c68583
Alexcey <alexcey007@yandex.ru>
2020-06-28 01:31:57 +0300

    add scope-methods

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
   has_many :tests
 
   validates :title, presence :true
+
+  default_scope { oreder(title: :asc) }
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -10,6 +10,12 @@ class Test < ApplicationRecord
   validates :title, presence: true,
                     uniqueness: {scope: :level}
 
+  scope :by_level, -> (level) { where(level: level) }
+
+  scope :easy, -> { where(level: 0..1) }
+  scope :normal, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+
   def category_where_title(title)
     Test.joins(:category).where('categories.title = ?', title).order(title: :desc).pluck(:title)
   end
