d7c771823a2f1972a95eb6180f3db3b3bfb6b16f
Alexcey <alexcey007@yandex.ru>
2020-10-22 01:48:45 +0300

    modif answer after scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :answers
+  #resources :answers
   root 'tests#index'
 
   resources :tests do
-    resources :questions, shallow: true
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true
+    end
   end
 end
