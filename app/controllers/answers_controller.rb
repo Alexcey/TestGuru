4aac449e9aa4c81cfaf33b1586c1b36703dff724
Alexcey <alexcey007@yandex.ru>
2020-10-26 22:57:02 +0300

    create answer in qestion

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,23 +1,18 @@
 class AnswersController < ApplicationController
 
-  before_action :find_question, only: %i[new create]
+  before_action :set_question, only: %i[new create]
   before_action :set_answer, only: %i[show edit update destroy]
 
-  def show
-  end
+  def show; end
 
   def new
     @answer = @question.answers.new
   end
 
-  # GET /answers/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /answers
-  # POST /answers.json
   def create
-    @answer = Answer.new(answer_params)
+    @answer = @question.answers.new(answer_params)
 
     if @answer.save
       redirect_to @answer, notice: 'Answer was successfully created.'
@@ -41,7 +36,7 @@ class AnswersController < ApplicationController
 
   private
 
-  def find_question
+  def set_question
     @question = Question.find(params[:question_id])
   end
 
