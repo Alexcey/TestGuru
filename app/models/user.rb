7086bb0ccda75d1f38bb35a4ea78265f6aec9a87
Alexcey <alexcey007@yandex.ru>
2020-11-10 02:04:36 +0300

    add devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,21 @@
 class User < ApplicationRecord
+
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :confirmable
+
   EMAIL_FORMAT = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i.freeze
 
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id'
 
-  validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true,
                    format: { with: EMAIL_FORMAT }
-
-  has_secure_password
 
   def tests_where_level(level)
     tests.where(level: level)
