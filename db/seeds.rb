e57f62c91a713f04209e7c55fc0e8eea67bbf408
Alexcey <alexcey007@yandex.ru>
2020-12-25 04:01:53 +0300

    external API, gems, gists

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,32 +18,32 @@ categories = Category.create!([
 #                         last_name: 'Last_name', first_name: 'First_name' }
 #                     ])
 
-#tests = Test.create!([
-#                       { category_id: categories[0].id, title: 'JS', level: 1, author: admin[0] },
-#                       { category_id: categories[1].id, title: 'Ruby', level: 2, author: admin[0] },
-#                       { category_id: categories[2].id, title: 'SQL', level: 3, author: admin[0] }
-#                     ])
+tests = Test.create!([
+                       { category_id: categories[0].id, title: 'JS', level: 1, author: User.first },
+                       { category_id: categories[1].id, title: 'Ruby', level: 2, author: User.first },
+                       { category_id: categories[2].id, title: 'SQL', level: 3, author: User.first }
+                     ])
 
-#questions = Question.create!([
-#                               { test_id: tests[0].id, body: 'JS is hard?' },
-#                               { test_id: tests[0].id, body: 'console.log(null == undefined)?' },
-#                               { test_id: tests[0].id, body: 'console.log(null === undefined)?' },
-#                               { test_id: tests[1].id, body: 'Ruby is beautiful?' },
-#                               { test_id: tests[2].id, body: 'SQL is best?' }
-#                             ])
+questions = Question.create!([
+                               { test_id: tests[0].id, body: 'JS is hard?' },
+                               { test_id: tests[0].id, body: 'console.log(null == undefined)?' },
+                               { test_id: tests[0].id, body: 'console.log(null === undefined)?' },
+                               { test_id: tests[1].id, body: 'Ruby is beautiful?' },
+                               { test_id: tests[2].id, body: 'SQL is best?' }
+                             ])
 
-#Answer.create!([
-#                 { question_id: questions[0].id, body: 'Yes', correct: true },
-#                 { question_id: questions[0].id, body: 'No', correct: false },
-#                 { question_id: questions[1].id, body: 'True', correct: true },
-#                 { question_id: questions[1].id, body: 'False', correct: false },
-#                 { question_id: questions[2].id, body: 'True', correct: false },
-#                 { question_id: questions[2].id, body: 'False', correct: true },
-#                 { question_id: questions[3].id, body: 'Yep!', correct: true },
-#                 { question_id: questions[4].id, body: 'No', correct: false },
-#                 { question_id: questions[4].id, body: 'Yes', correct: true },
-#                 { question_id: questions[4].id, body: 'No', correct: false }
-#               ])
+Answer.create!([
+                 { question_id: questions[0].id, body: 'Yes', correct: true },
+                 { question_id: questions[0].id, body: 'No', correct: false },
+                 { question_id: questions[1].id, body: 'True', correct: true },
+                 { question_id: questions[1].id, body: 'False', correct: false },
+                 { question_id: questions[2].id, body: 'True', correct: false },
+                 { question_id: questions[2].id, body: 'False', correct: true },
+                 { question_id: questions[3].id, body: 'Yep!', correct: true },
+                 { question_id: questions[4].id, body: 'No', correct: false },
+                 { question_id: questions[4].id, body: 'Yes', correct: true },
+                 { question_id: questions[4].id, body: 'No', correct: false }
+               ])
 
 # TestPassage.create!([
 #                       { user_id: users[0].id, test_id: tests[0].id, current_question_id: questions[2].id },
