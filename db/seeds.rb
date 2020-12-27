6eaa9497829c045f1131f3437bc160d47672001c
Alexcey <alexcey007@yandex.ru>
2020-12-27 04:32:54 +0300

    add sort and icon confirm passord

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,15 @@ admin = Admin.create!(password: '123123', email: 'alex12@ya.ru',
                       first_name: 'Admin', last_name: 'Admin')
 
 tests = Test.create!([
-                       { category_id: categories[0].id, title: 'JS', level: 1, author: admin.id },
-                       { category_id: categories[1].id, title: 'Ruby', level: 2, author: admin.id },
-                       { category_id: categories[2].id, title: 'SQL', level: 3, author: admin.id }
+                       { category_id: categories[0].id, title: 'JS', level: 1, author_id: admin.id },
+                       { category_id: categories[1].id, title: 'Ruby', level: 2, author_id: admin.id }
                      ])
 
 questions = Question.create!([
                                { test_id: tests[0].id, body: 'JS is hard?' },
                                { test_id: tests[0].id, body: 'console.log(null == undefined)?' },
                                { test_id: tests[0].id, body: 'console.log(null === undefined)?' },
-                               { test_id: tests[1].id, body: 'Ruby is beautiful?' },
-                               { test_id: tests[2].id, body: 'SQL is best?' }
+                               { test_id: tests[1].id, body: 'Ruby is beautiful?' }
                              ])
 
 Answer.create!([
@@ -30,15 +28,7 @@ Answer.create!([
                  { question_id: questions[2].id, body: 'True', correct: false },
                  { question_id: questions[2].id, body: 'False', correct: true },
                  { question_id: questions[3].id, body: 'Yep!', correct: true },
-                 { question_id: questions[4].id, body: 'No', correct: false },
-                 { question_id: questions[4].id, body: 'Yes', correct: true },
-                 { question_id: questions[4].id, body: 'No', correct: false }
+                 { question_id: questions[3].id, body: 'No', correct: false }
                ])
-
-# TestPassage.create!([
-#                       { user_id: users[0].id, test_id: tests[0].id, current_question_id: questions[2].id },
-#                       { user_id: users[0].id, test_id: tests[0].id, current_question_id: questions[2].id },
-#                       { user_id: users[2].id, test_id: tests[1].id, current_question_id: questions[2].id }
-#                     ])
 
 puts 'end db:seed'
